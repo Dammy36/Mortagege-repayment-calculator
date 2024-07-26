b25ebe4aa89ca17b5d4f5f9d785f60f9b04da156
Dammy36 <osedee101@gmail.com>
2024-07-26 15:04:10 +0100

    Added event listener to the button

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 function buttonClick() {
-  let secondInputbutton = document.querySelector(".second-input-button");
+  let borderA = document.getElementById("border-A");
+  console.log(borderA);
+  let borderB = document.getElementById("border-B");
 
-  let sectionParagraph = document.querySelector(".section__paragraph");
-
-  secondInputbutton.addEventListener("click", function () {
-    sectionParagraph.classList.toggle("open");
-  });
+  if (borderA.style.display !== "none") {
+    borderA.style.display = "none";
+    borderB.style.display = "block";
+  } else {
+    borderA.style.display = "block";
+    borderB.style.display = "none";
+  }
 }
-buttonClick();
+
+const projectInput = document.querySelector(".project-input");
+const amountButton = document.querySelector(".Amount-button");
+
+projectInput.addEventListener("click", function () {
+  if (amountButton.style.backgroundColor === "var(--secondry-color)") {
+    amountButton.style.backgroundColor = "";
+  } else {
+    amountButton.style.backgroundColor = "var(--secondry-color)";
+  }
+});
